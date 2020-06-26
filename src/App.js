a205c459b1460ef01aa0573a14e454c256374ad5
kshitij naresh <kshitijhack@gmail.com>
2020-06-26 08:08:18 +0530

    last changes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import AllPost from './AllPost';
 
 
 class App extends Component {
-  render() {
-    return (
-    <div className="App">
-        <PostForm />
-        <AllPost />
+render() {
+return (
+<div className="App">
+  <div className="navbar">
+    <h2 className="center ">Post It</h2>
     </div>
-    );
-    }
-  }
+    <PostForm />
+    <AllPost />
+</div>
+);
+}
+}
 export default App;
