7195a532f077ae42c6a6a7681ec7dc91f007694d
kshitij naresh <kshitijhack@gmail.com>
2020-06-26 05:48:27 +0530

    adding the state as props to allform

--- src/AllPost.js
+++ src/AllPost.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
 class AllPost extends Component {
-  render() {
-    return (
-    <div>
-      <h1>All Posts</h1>
-    </div>
-    );
-   }
+    render() {
+        return (
+            <div>
+                <h1>All Posts</h1>
+                {console.log(this.props.posts)}
+            </div>
+        );
+    }
 }
 
-export default AllPost;
+const mapStateToProps = (state) => {
+    return {
+        posts: state
+    }
+}
+export default connect(mapStateToProps)(AllPost);
